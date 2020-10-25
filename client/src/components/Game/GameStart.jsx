061e0035c5ba968bc84e839550bf2ef24b81f055
Rita Mwaikinda <srmwaikinda@gmail.com>
2020-10-25 12:35:21 -0400

    game landing page jsx and css

---
+++ client/src/components/Game/GameStart.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import './GameStart.css';
+
+const GameStart = () => {
+
+    return(
+        <div class="gamestart">
+          <h1 class="title">Test Your Knowledge</h1>
+
+          <div class="startquiz">
+
+            <h2>Game Rules</h2>
+
+            <div class="rulesbox">
+               
+                <ol class>
+                  <li>There is 4 characters. Click on the character the quote applies to.</li>
+                  <li>Each player is only allowed 2 wrong answers</li>
+                  <li>Try to reach the highest score!</li>
+                  <li>If you need some review, hit the character index above<img id="arrow" src="https://www.flaticon.com/svg/static/icons/svg/65/65387.svg" alt="arrow" /> to refresh on your Breaking Bad!!!</li>
+                </ol>
+            </div>
+            <div class="box">  
+              <button class="startgame" href="./Game">START</button>
+            </div>
+          </div>
+        </div>
+    )
+
+
+    
+}
+export default GameStart

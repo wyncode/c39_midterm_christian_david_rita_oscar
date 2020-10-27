b886c5589b801c78afefeaf37abdbd7b7b07ddeb
oscarporfer <oscarfportes@gmail.com>
2020-10-26 21:45:47 -0400

    CSS edits

--- client/src/components/LandingPage/Carousel/Carousel.jsx
+++ client/src/components/LandingPage/Carousel/Carousel.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
-import './Carousel.css';
+import React, {Component} from 'react'
 
-function Carousel() {
-  return (
-    <div className="carousel">
-      <h1>Carousel here</h1>
-    </div>
-  );
+
+class Carousel extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <div className='view-port' style={StyleSheet.view_port}>
+          <div className='card-container' style={StyleSheet.card_container}>
+             
+
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
+const styles = {
+  view_port : {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '350px',
+    height: '200px',
+    backgroundColor: 'red'
+
+  },
+
+  card_container: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: 'fit-content',
+  }
+
+}
 export default Carousel;
